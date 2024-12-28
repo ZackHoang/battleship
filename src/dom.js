6bd8e239247f4489a90fcf220fb88d529c2f6406
Zack <zackhoang2004@gmail.com>
2024-12-28 16:36:41 +0700

    Add ship placement feature
    Squares can now be hovered to visualize where ships can be placed. Randomizing placements are left out for now. Need to work on handling error for out of bound color change

--- src/dom.js
+++ src/dom.js
@@ -1,38 +1,65 @@
 const playerBoardContainer = document.querySelector("#player_board");
 const playerGrid = playerBoardContainer.getElementsByClassName("grid"); 
-const playerBoard = []; 
 let playerGridIndex = 0; 
+export const playerBoard = []; 
 for (let row = 0; row < 10; row++) {
-    playerBoard.push([]);  
+    playerBoard.push([]); 
     for (let col = 0; col < 10; col++) {
         playerBoard[row].push(playerGrid[playerGridIndex]); 
         playerGridIndex++; 
     }
 }
+console.log(playerBoard); 
 
-export function renderBoard() {
-    const description = document.querySelector("#description"); 
-    description.textContent = "Place your ships"; 
-    console.log(playerBoard); 
-    const shipsLength = [5, 4, 3, 3, 2];  
+export let shipsLength = [5, 4, 3, 3, 2]; 
+export let shipsLengthIndex = 0; 
+let horizontal = true; 
 
-    for (let i = 0; i < shipsLength.length; i++) {
-        let randomRow = Math.floor(Math.random() * 10); 
-        let randomCol = Math.floor(Math.random() * 10);
-        const randomVertical = false;  
-        if (!randomVertical) {
-            let colIter = []; 
-            for (let j = 0; j < shipsLength[i]; j++) {
-                colIter.push(randomCol + j); 
-            }
-            if (colIter.some((col) => col > 9)) {
-                const difference = colIter[colIter.length - 1] - 9; 
-                randomCol -= difference; 
-            }
-            for (let j = 0; j < shipsLength[i]; j++) {
-                playerBoard[randomRow][randomCol + j].style.backgroundColor = "green"; 
-            }
+export function placeShip () {
+    for (let row = 0; row < 10; row++) {
+        for (let col = 0; col < 10; col++) {
+            playerBoard[row][col].removeEventListener("mouseenter", placeShip); 
+            playerBoard[row][col].removeEventListener("mouseleave", placeShip); 
+        }
+    }
+
+    for (let row = 0; row < 10; row++) {
+        for (let col = 0; col < 10; col++) {
+            playerBoard[row][col].addEventListener("mouseenter", (e) => {
+                e.target.style.backgroundColor = "green"; 
+                if (horizontal === true) {
+                    for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
+                        playerBoard[row][addCol].style.backgroundColor = "green"; 
+                    }
+                } else {
+                    for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
+                        playerBoard[addRow][col].style.backgroundColor = "green"; 
+                    }
+                }
+                
+            }); 
+    
+            playerBoard[row][col].addEventListener("mouseleave", (e) => {
+                e.target.style.backgroundColor = "unset"; 
+                if (horizontal === true) {
+                    for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
+                        playerBoard[row][addCol].style.backgroundColor = "unset"; 
+                    }
+                } else {
+                    for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
+                        playerBoard[addRow][col].style.backgroundColor = "unset"; 
+                    }
+                }
+            }); 
         }
     }
 }
 
+export function changeOrientation() {
+    if (horizontal === true) {
+        horizontal = false; 
+    } else {
+        horizontal = true; 
+    }    
+}
+ 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import "./styles.css";
 import "./normalize.css"; 
 import { Player } from "./Player";
-import { renderBoard } from "./dom";
+import { changeOrientation, placeShip } from "./dom";
 
 const playerName = prompt("Welcome to Battleship! Enter your name: ", "Player 1"); 
 const playerNameDisplay = document.querySelector("#player"); 
 playerNameDisplay.textContent = playerName; 
 export const player = new Player(playerName); 
 export const computer = new Player("Computer");
-export const shipsLocation = []; 
 
-renderBoard(); 
+
+placeShip(); 
+const btn = document.querySelector("button"); 
+btn.addEventListener("click", () => {
+    changeOrientation(); 
+}); 
