d97113e96c84ae9594667325974ce91ca5e9af9e
Zack <zackhoang2004@gmail.com>
2024-12-29 10:29:34 +0700

    Add overflow error handling
    There will now be an alert when the cursor is moved to a square that causes potential overflow. It will then exit out of the placeShip function within the DOM

--- src/dom.js
+++ src/dom.js
@@ -1,7 +1,7 @@
 const playerBoardContainer = document.querySelector("#player_board");
 const playerGrid = playerBoardContainer.getElementsByClassName("grid"); 
 let playerGridIndex = 0; 
-export const playerBoard = []; 
+const playerBoard = []; 
 for (let row = 0; row < 10; row++) {
     playerBoard.push([]); 
     for (let col = 0; col < 10; col++) {
@@ -29,11 +29,21 @@ export function placeShip () {
                 e.target.style.backgroundColor = "green"; 
                 if (horizontal === true) {
                     for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
-                        playerBoard[row][addCol].style.backgroundColor = "green"; 
+                        if (addCol > 9) {
+                            alert("This space is not available"); 
+                            return; 
+                        } else {
+                            playerBoard[row][addCol].style.backgroundColor = "green"; 
+                        }
                     }
                 } else {
                     for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
-                        playerBoard[addRow][col].style.backgroundColor = "green"; 
+                        if (addRow > 9) {
+                            alert("This space is not available"); 
+                            return;  
+                        } else {
+                            playerBoard[addRow][col].style.backgroundColor = "green"; 
+                        }
                     }
                 }
                 
@@ -43,11 +53,19 @@ export function placeShip () {
                 e.target.style.backgroundColor = "unset"; 
                 if (horizontal === true) {
                     for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
-                        playerBoard[row][addCol].style.backgroundColor = "unset"; 
+                        if (addCol > 9) {
+                            return; 
+                        } else {
+                            playerBoard[row][addCol].style.backgroundColor = "unset"; 
+                        }
                     }
                 } else {
                     for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
-                        playerBoard[addRow][col].style.backgroundColor = "unset"; 
+                        if (addRow > 9) {
+                            return; 
+                        } else {
+                            playerBoard[addRow][col].style.backgroundColor = "unset"; 
+                        }
                     }
                 }
             }); 
