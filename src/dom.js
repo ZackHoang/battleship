7ba0d593f356b4ea308ff8c9c44d1b0946661955
Zack <zackhoang2004@gmail.com>
2024-12-30 20:26:24 +0700

    Update Fix Remove Event Listener Error

--- src/dom.js
+++ src/dom.js
@@ -17,53 +17,55 @@ export let shipsLength = [5, 4, 3, 3, 2];
 export let shipsLengthIndex = 0; 
 let horizontal = true; 
 
+function highlightShip (event) {
+    event.target.style.backgroundColor = "green"; 
+    if (horizontal === true) {
+        for (let addCol = parseInt(event.target.dataset.col) + 1; addCol < parseInt(event.target.dataset.col) + shipsLength[shipsLengthIndex]; addCol++) {
+            if (addCol > 9) {
+                alert("This space is not available"); 
+                return; 
+            } else {
+                playerBoard[parseInt(event.target.dataset.row)][addCol].style.backgroundColor = "green"; 
+            }
+        }
+    } else {
+        for (let addRow = parseInt(event.target.dataset.row) + 1; addRow < parseInt(event.target.dataset.row) + shipsLength[shipsLengthIndex]; addRow++) {
+            if (addRow > 9) {
+                alert("This space is not available"); 
+                return;  
+            } else {
+                playerBoard[addRow][parseInt(event.target.dataset.col)].style.backgroundColor = "green"; 
+            }
+        }
+    }
+}
+
+function stopHighlightShip (event) {
+    event.target.style.backgroundColor = "unset"; 
+    if (horizontal === true) {
+        for (let addCol = parseInt(event.target.dataset.col) + 1; addCol < parseInt(event.target.dataset.col) + shipsLength[shipsLengthIndex]; addCol++) {
+            if (addCol > 9) {
+                return; 
+            } else {
+                playerBoard[parseInt(event.target.dataset.row)][addCol].style.backgroundColor = "unset"; 
+            }
+        }
+    } else {
+        for (let addRow = parseInt(event.target.dataset.row) + 1; addRow < parseInt(event.target.dataset.row) + shipsLength[shipsLengthIndex]; addRow++) {
+            if (addRow > 9) {
+                return; 
+            } else {
+                playerBoard[addRow][parseInt(event.target.dataset.col)].style.backgroundColor = "unset"; 
+            }
+        }
+    }
+}
+
 export function placeShip () {
     for (let row = 0; row < 10; row++) {
         for (let col = 0; col < 10; col++) {
-            playerBoard[row][col].addEventListener("mouseenter", (e) => {
-                e.target.style.backgroundColor = "green"; 
-                if (horizontal === true) {
-                    for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
-                        if (addCol > 9) {
-                            alert("This space is not available"); 
-                            return; 
-                        } else {
-                            playerBoard[row][addCol].style.backgroundColor = "green"; 
-                        }
-                    }
-                } else {
-                    for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
-                        if (addRow > 9) {
-                            alert("This space is not available"); 
-                            return;  
-                        } else {
-                            playerBoard[addRow][col].style.backgroundColor = "green"; 
-                        }
-                    }
-                }
-                
-            }); 
-    
-            playerBoard[row][col].addEventListener("mouseleave", (e) => {
-                e.target.style.backgroundColor = "unset"; 
-                if (horizontal === true) {
-                    for (let addCol = col + 1; addCol < col + shipsLength[shipsLengthIndex]; addCol++) {
-                        if (addCol > 9) {
-                            return; 
-                        } else {
-                            playerBoard[row][addCol].style.backgroundColor = "unset"; 
-                        }
-                    }
-                } else {
-                    for (let addRow = row + 1; addRow < row + shipsLength[shipsLengthIndex]; addRow++) {
-                        if (addRow > 9) {
-                            return; 
-                        } else {
-                            playerBoard[addRow][col].style.backgroundColor = "unset"; 
-                        }
-                    }
-                }
-            }); 
+            playerBoard[row][col].addEventListener("mouseenter", highlightShip)  
+            playerBoard[row][col].addEventListener("mouseleave", stopHighlightShip); 
         }
     }
 }
@@ -80,14 +82,10 @@ export function confirmShip () {
                     alert("A ship is already placed here")
                 }
 
-                for (let row = 0; row < 10; row++) {
-                    for (let col = 0; col < 10; col++) {
-                        if (player.board.board[row][col] === 1) {
-                            playerBoard[row][col].style.backgroundColor = "yellow"; 
-                            playerBoard[row][col].removeEventListener("mouseenter", placeShip); 
-                            playerBoard[row][col].removeEventListener("mouseleave", placeShip); 
-                        }
-                    }
+                if (player.board.board[row][col] === 1) {
+                    e.target.style.backgroundColor = "yellow";
+                    e.target.removeEventListener("mouseenter", highlightShip); 
+                    e.target.removeEventListener("mouseleave", stopHighlightShip);
                 }
             });
         } 
